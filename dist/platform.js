67c0d4d4351a38a139d9fffb6e8e136f4f922ba4
Markus Teichmann <markus.teichmann@polizei.niedersachsen.de>
2020-04-01 15:39:50 +0200

    step

--- dist/platform.js
+++ dist/platform.js
@@ -255,7 +255,7 @@ var Platform = /*#__PURE__*/function () {
             slotRotationalArea: parseInt(sumState[4])
           };
 
-          _this4.log.debug("Status position", item, position, state.position); // Update service
+          _this4.log("Status position", item, position, state.position); // Update service
 
 
           if (state.position != _this4.blinds[item].position) {
--- src/platform.js
+++ src/platform.js
@@ -189,7 +189,7 @@ class Platform {
           sumState: parseInt(sumState[3]),
           slotRotationalArea: parseInt(sumState[4])
         };
-        this.log.debug("Status position", item, position, state.position);
+        this.log("Status position", item, position, state.position);
         // Update service
         if (state.position != this.blinds[item].position) {
           const { Service, Characteristic } = this.api.hap;
