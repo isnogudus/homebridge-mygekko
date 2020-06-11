af8d71af49a7278431b085ceef2c69d898cd2e3e
Markus Teichmann <markus.teichmann@polizei.niedersachsen.de>
2020-06-11 12:23:29 +0200

    step

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ export default (homebridge) => {
   const PluginName = 'homebridge-mygekko';
   const PlatformName = 'mygekko';
 
-  homebridge.registerPlatform(PluginName, PlatformName, Platform);
+  homebridge.registerPlatform(PlatformName, Platform);
 };
